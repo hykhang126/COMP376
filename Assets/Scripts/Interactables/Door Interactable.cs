92b83ab1d9259a8998aca118130a55d47e93ef63
Abdallah Said <amhs2200@gmail.com>
2025-07-31 16:10:55 -0400

    Added the ability to load a level async on a door being opened
    Also added a dict in the door interactable script to determine what key opens which scene as well as an array to determine which key can open this specific door

--- Assets/Scripts/Interactables/Door Interactable.cs
+++ Assets/Scripts/Interactables/Door Interactable.cs
@@ -1,16 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class DoorInteractable : Interactable
 {
     
-    [SerializeField] private int itemKey;
+    [SerializeField] private int[] itemKeysToOpenThisDoor;
+
+    private AsyncOperation loadNextScene;
+
+    private Dictionary<int, string> itemKeyToSceneName = new Dictionary<int, string>
+    {
+        // Insert the rest of the key item keys here alongside the scene name they are supposed to open
+        {101, "Scene 1"}
+    };
     //[SerializeField] private Texture2D inventoryImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Item created with key: " + itemKey);
+        Debug.Log("Item created with key: " + itemKeysToOpenThisDoor);
     }
 
     public override void Interact(Player player)
@@ -19,15 +30,20 @@ public class DoorInteractable : Interactable
         {
             if (player.inventory)
             {
-                if (player.inventory.UseItem(itemKey))
+                for (int i = 0; i < itemKeysToOpenThisDoor.Length; i++)
                 {
-                    //TODO: Play open door animation
-                    //TODO: Load next level
-                    Debug.Log("Door Open");
-                }
-                else
-                {
-                    Debug.Log("Player doesn't have a key in their inventory!");
+                    //----- To be replaced by getting the equipped key and checking if it is in the array -----
+                    if (player.inventory.UseItem(itemKeysToOpenThisDoor[i]))
+                    {
+                        loadNextScene = SceneManager.LoadSceneAsync(itemKeyToSceneName[itemKeysToOpenThisDoor[i]], LoadSceneMode.Additive);
+                        loadNextScene.completed += UnlockDoor;
+                        break;
+                        //-------------------------------------------------------------------------------------
+                    }
+                    else
+                    {
+                        Debug.Log("Player doesn't have a key in their inventory!");
+                    }
                 }
             }
             else
@@ -40,5 +56,11 @@ public class DoorInteractable : Interactable
             Debug.LogError("Player's inventory is null, cannot add item.");
             return;
         }
+    }
+
+    private void UnlockDoor(AsyncOperation op)
+    {
+        //TODO: Door Opening anim and sound
+        Debug.Log("Door Open");
     }
 }
