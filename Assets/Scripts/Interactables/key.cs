94d68c877b058e3d8874f56130b54efaf0bba5ef
Charles Partous <charles.partous@gmail.com>
2025-09-28 13:09:21 -0400

    Modify Key8 script for new interaction implementation and add InteractableComponent to prefab

---
+++ Assets/Scripts/Interactables/key.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<InteractableComponent>().interactionEntered.AddListener(OnInteractEntered);
+        GetComponent<InteractableComponent>().interactionTriggered.AddListener(OnInteractTriggered);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnInteractEntered()
+    {
+        Debug.Log("Key Interact Enter");
+        //Hover code here
+    }
+
+    void OnInteractTriggered()
+    {
+        Debug.Log("Key Interact Triggered");
+        //Interaction code here
+    }
+}
