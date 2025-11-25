4386f50cf64cb17d0969f89ac84e232456419a0f
Ricky2203 <rcaucci8@gmail.com>
2025-11-25 16:26:34 -0500

    Adding drawer + extra door locked sfx for secret room

--- Assets/ASSETS_EXTERNAL/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
+++ Assets/ASSETS_EXTERNAL/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Drawer/X Axis/Drawer_Pull_X.cs
@@ -3,38 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace SojaExiles
-
 {
-
 	public class Drawer_Pull_X : MonoBehaviour
 	{
-
 		public Animator pull_01;
 		public bool open;
 		public Transform Player;
 
+		[Header("SFX (simple)")]
+		public AudioClip openSFX;
+		public AudioClip closeSFX;
+		[Range(0f, 1f)]
+		public float sfxVolume = 1f;
+
+		private AudioSource audioSource;
+
 		void Start()
 		{
 			open = false;
+
+			// ensure there's an AudioSource to play SFX
+			audioSource = GetComponent<AudioSource>();
+			if (audioSource == null)
+			{
+				audioSource = gameObject.AddComponent<AudioSource>();
+				audioSource.playOnAwake = false;
+			}
 		}
 
 		public void OpenorClose()
 		{
-
 			if (!open)
 			{
 				StartCoroutine(opening());
 			}
-            else
-            {
+			else
+			{
 				StartCoroutine(closing());
-            }
-        }
+			}
+		}
 
 		IEnumerator opening()
 		{
 			print("you are opening the door");
 			pull_01.Play("openpull_01");
+
+			// play open SFX
+			if (openSFX != null && audioSource != null)
+				audioSource.PlayOneShot(openSFX, sfxVolume);
+
 			open = true;
 			yield return new WaitForSeconds(.5f);
 		}
@@ -43,10 +60,13 @@ namespace SojaExiles
 		{
 			print("you are closing the door");
 			pull_01.Play("closepush_01");
+
+			// play close SFX
+			if (closeSFX != null && audioSource != null)
+				audioSource.PlayOneShot(closeSFX, sfxVolume);
+
 			open = false;
 			yield return new WaitForSeconds(.5f);
 		}
-
-
 	}
 }
