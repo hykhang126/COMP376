a649b08925f81751b3ffa127ff9d10f727974a56
Adam Farahat <farahatadam@gmail.com>
2025-11-26 00:50:46 -0500

    Colors change on start, except for one of them in frame checker

--- Assets/Scripts/Events/Painting_FrameChecker.cs
+++ Assets/Scripts/Events/Painting_FrameChecker.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor.SpeedTree.Importer;
 using UnityEngine;
 
 public class FrameChecker : MonoBehaviour
@@ -11,6 +13,10 @@ public class FrameChecker : MonoBehaviour
 
     public bool hasSpawnedKey = false;
 
+    [SerializeField] private Material[] requiredMaterials;
+    
+    private List<Material> colorMaterials;
+
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
@@ -19,19 +25,37 @@ public class FrameChecker : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+        colorMaterials = new List<Material>(requiredMaterials);
         if (frameRenderers.Length == 0)
         {
             Debug.LogWarning("No frame renderers assigned to EV2_FrameChecker.");
             return;
         }
 
-        foreach (var renderer in frameRenderers)
+        for(int i=0;i<frameRenderers.Length;i++)
         {
-            if (renderer == null)
+            Renderer renderer = frameRenderers[i];
+            if (frameRenderers[i] == null || materialsToCheck[i] == null)
             {
-                Debug.LogWarning("One of the frame renderers is not assigned.");
+                Debug.LogWarning("One of the frame renderers or materials to check is not assigned.");
                 return;
             }
+
+            int randomIndex = Random.Range(0, colorMaterials.Count);
+            int randomIndexToCheck = Random.Range(0, colorMaterials.Count);
+
+            while(randomIndexToCheck == randomIndex)
+            {
+                randomIndexToCheck = Random.Range(0, colorMaterials.Count);
+            }
+            frameRenderers[i].material.color = colorMaterials[randomIndex].color;
+            materialsToCheck[i].material.color = colorMaterials[randomIndexToCheck].color;
+
+            Debug.Log("Assigned color " + frameRenderers[i].material.color + " to frame renderer " + i);
+            Debug.Log("Assigned color " + materialsToCheck[i].material.color + " to material to check " + i);
+
+            colorMaterials.RemoveAt(randomIndex); // Ensure unique colors
         }
 
         keySpawner = transform.Find("KeySpawner")?.gameObject;
@@ -44,7 +68,7 @@ public class FrameChecker : MonoBehaviour
     {
         for (int i = 0; i < frameRenderers.Length; i++)
         {
-            if (frameRenderers[i].material.name != materialsToCheck[i].material.name)
+            if (frameRenderers[i].material.name != materialsToCheck[i].material.name && frameRenderers[i].material.color != materialsToCheck[i].material.color)
             {
                 isCorrectMaterial = false; // If any material does not match
                 return;
